6b5ed33a3196a77021da6188a25511e3d30c5cd8
jasonsanders <jas.sanders@gmail.com>
2016-12-03 11:41:46 -0700

    finished project, but order isn't working

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Friend extends Component {
+
+  render() {
+    return(
+      <li className='friend'>
+      <img className="profile-pic" src={ this.props.pictureUrl } />
+        <h3>{ this.props.name }</h3>
+        <div className="location">
+            Location: { this.props.currentLocation.city }, { this.props.currentLocation.state }, { this.props.currentLocation.country }
+        </div>
+        <div className="status">
+            Message: { this.props.status }
+        </div>
+        <div className="num-friends">
+            Friends: { this.props.friendCount }
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Friend;
